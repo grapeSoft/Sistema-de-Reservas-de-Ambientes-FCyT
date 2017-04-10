fab3f8218e393318c12c83fcfe9825cfb48a4be9
diegovillasolis <diego.villarroel@outlook.com>
2017-04-10 11:07:54 -0400

    rutas para visualizar foto perfil

--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,20 @@
 |
 */
 
-Route::get('/', 'PrincipalController@inicio')->name('principal.inicio');
-
 Route::get('login', 'UsuarioController@login')->name('usuarios.login');
+
+//validara los datos del login
 Route::post('logear', 'UsuarioController@logear')->name('usuarios.logear');
-Route::get('logout', 'UsuarioController@logout')->name('usuarios.logout');
-Route::get('usuarios/perfil', 'UsuarioController@perfil')->name('usuarios.perfil');
-Route::get('usuarios/{id}/foto', 'UsuarioController@foto')
-    ->name('usuarios.foto');
-Route::resource('usuarios', 'UsuarioController');
+
+//rutas que se mostraran cuando el usuario inicie sesion
+Route::group(['middleware' => 'autentificado'], function(){
+	Route::get('/', 'PrincipalController@inicio')->name('principal.inicio');
+	Route::get('logout', 'UsuarioController@logout')->name('usuarios.logout');
+	Route::get('usuarios/perfil', 'UsuarioController@perfil')->name('usuarios.perfil');
+	Route::get('usuarios/{id}/foto', 'UsuarioController@foto')
+	    ->name('usuarios.foto');
+	Route::resource('usuarios', 'UsuarioController');	
+});
 
 
 
