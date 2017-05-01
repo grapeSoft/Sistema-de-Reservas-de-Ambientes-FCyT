9ec38384f7d5fb6bd1190adc600d0098787a4e90
markoz1 <marco.jhoel@gmail.com>
2017-05-01 18:38:08 -0400

    formualario autorizado con laravelCollective

--- resources/views/reservas/formularios/autorizado.blade.php
+++ resources/views/reservas/formularios/autorizado.blade.php
@@ -1,20 +1,15 @@
 <div class="row">
 	<div class="form-group" style="margin-top: 0;">
-		<label for="select" class="control-label col-md-3">Tipo de Reserva</label>
+		{!! Form::label('tipo', 'Tipo de Reservas', ['class' => 'control-label col-md-3']) !!}
 		<div class="col-md-12">
-			<select id="select" class="form-control" placeholder="asdfasd">
-				<option>Congreso</option>
-				<option>Seminario</option>
-				<option>Cursos</option>
-				<option>Charlas</option>
-			</select>
-		</div>
+			{!! Form::select('tipo', ['Congreso', 'Seminario', 'Cursos', 'Charlas', 'otros'], null, ['class' => 'form-control'])!!}
+		</div> 
 	</div>
 </div>
 <div class="row">
 	<div class="form-group">
 		<div class="col-md-12">
-			<textarea class="form-control" rows="3" id="textArea" placeholder="Descripción..."></textarea>
+			{!! Form::textarea('descripcion', null, ['class' => 'form-control', 'placeholder' => 'Descripción...', 'rows' => '3']) !!}
 			<span class="help-block">Escribe una breve descripción sobre el tipo de reserva que realices.</span>
 		</div>
 	</div>
