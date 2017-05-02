f31dd6709b43146e6fe8cd19c888a4a534fe705b
markoz1 <marco.jhoel@gmail.com>
2017-05-01 20:01:08 -0400

    correcciones menores vista

--- resources/views/reservas/create/create.blade.php
+++ resources/views/reservas/create/create.blade.php
@@ -38,12 +38,4 @@
 </div>
 @endsection
 @section('panel-footer')
-<div class="btn-group btn-group-justified">
-    <div class="col-md-2 col-md-offset-10">
-        <div class="btn-group">
-            <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#formularioReserva">Siguiente</button>
-        </div>
-    </div>
-</div>
-    @include('reservas.formularios.form')
 @endsection
