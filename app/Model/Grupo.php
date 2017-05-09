d84ec7548a61ade8d888652e450bf780826dcbb4
diegovillasolis <diego.villarroel@outlook.com>
2017-05-08 23:27:14 -0400

    Validaciones estaticas para reservas

--- app/Model/Grupo.php
+++ app/Model/Grupo.php
@@ -6,15 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Grupo extends Model
 {
-    protected $table = 'grupo';
-    protected $primaryKey = 'id_grupo';
-
-    public $fillable = [
-        'id_materia', 'numero'
-    ];
-
-    public function peteneceMateria()
-    {
-    	return $this->belongsTo('app/Model/Materia');
-    }
+    protected $table = 'usuario_materia';
+    protected $primaryKey = 'id_usuario_materia';
 }
