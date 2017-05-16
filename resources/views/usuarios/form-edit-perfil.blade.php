2fc9db73edca202afacaaa87a8d010b9970a454b
markoz1 <marco.jhoel@gmail.com>
2017-05-15 21:33:46 -0400

    correciones subir foto de usuario

--- resources/views/usuarios/form-edit-perfil.blade.php
+++ resources/views/usuarios/form-edit-perfil.blade.php
@@ -1,4 +1,18 @@
 @include('usuarios.form-basic')
+@if($errors->has('foto'))
+    <div class="form-group has-error">
+        <div class="help-block">
+            {{ $errors->first('foto') }}
+        </div>
+        @else
+            <div class="form-group">
+                @endif
+                {!! Form::label('foto', 'Foto', ['class' => 'control-label col-md-2']) !!}
+                <div class="col-md-10">
+                    <input type="text" readonly="" class="form-control" placeholder="Subir imagen...">
+                    {!! Form::file('foto', ['class' => 'form-control']) !!}
+                </div>     
+            </div>
 @if($errors->has('username'))
     <div class="form-group has-error">
         <div class="help-block">
