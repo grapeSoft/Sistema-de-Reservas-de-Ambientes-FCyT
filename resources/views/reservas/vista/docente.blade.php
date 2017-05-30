27916cc46be6ba817db5cee1b8bc8509619b7a6b
markoz1 <marco.jhoel@gmail.com>
2017-05-30 02:38:55 -0400

    correcciones detalle reserva docente

--- resources/views/reservas/vista/docente.blade.php
+++ resources/views/reservas/vista/docente.blade.php
@@ -1,23 +1,35 @@
-		   <table class="table table-bordered table-hover">
-					<tr>
-						<td><strong>Id. de Usuario</strong></td>
-						<td>{{ $datosUsuario->id_usuario }}</td>
-					</tr>
-					<tr>
-						<td><strong>Nombre</strong></td>
-						<td>{{ $datosUsuario->nombre }} {{ $datosUsuario->apellido_paterno }} {{ $datosUsuario->apellido_materno }} </td>
-					</tr>
-					<tr>
-						<td><strong>Email</strong></td>
-						<td>{{ $datosUsuario->email }}</td>
-					</tr>
-
-					<tr>
-						<td><strong>Materias</strong></td>				               
-                        @foreach($eventos as $evento)<td> {{ $evento->nombre }} --> Grupo {{ $evento->grupo }} </td><tr>
-                        </tr> <td></td> @endforeach                                   
-                    </tr> 
-
-					
-				</table>
+<table class="table table-bordered table-hover">
+	<tr>
+		<td><strong>Id. de Usuario</strong></td>
+		<td>{{ $datosUsuario->id_usuario }}</td>
+	</tr>
+	<tr>
+		<td><strong>Nombre</strong></td>
+		<td>{{ $datosUsuario->nombre }} {{ $datosUsuario->apellido_paterno }} {{ $datosUsuario->apellido_materno }} </td>
+	</tr>
+	<tr>
+		<td><strong>Email</strong></td>
+		<td>{{ $datosUsuario->email }}</td>
+	</tr>
+	<tr>
+		<td><strong>Tipo de Reserva</strong></td>
+		<td>Examen</td>
+	</tr>
+</table>
+<table class="table table-bordered table-hover">
+	<thead>
+		<tr>
+		<th>Materia</th>
+		<th>Grupo</th>
+		</tr>
+	</thead>
+	<tbody>
+	@foreach($eventos as $evento)
+		<tr>
+		<td>{{ $evento->nombre }}</td>
+		<td>{{ $evento->grupo }}</td>
+		</tr>
+	@endforeach                                   
+	</tbody>
+</table>
 			
