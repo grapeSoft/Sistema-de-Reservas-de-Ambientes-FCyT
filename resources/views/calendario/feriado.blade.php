0f6880d665707cf4250ee169a6da1087fe122ac2
Diego Villarroel Solis <diego.villarroel@outlook.com>
2017-06-06 02:31:51 -0400

    Configuracion del calendario sin validaciones de campos fechas

--- resources/views/calendario/feriado.blade.php
+++ resources/views/calendario/feriado.blade.php
@@ -3,22 +3,38 @@
 @section('contenido-principal-calendario')
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
+			{!! Form::open(['route' => ['calendario.updateFeriado'],'method' => 'post','files' => true,'class' => 'form-horizontal']) !!}
 			<div class="col-md-6">
 				<div class="form-group">
-			        {!! Form::label('titulo', 'Nombre feriado', ['class' => 'control-label col-md-4']) !!}
-			        <div class="col-md-8">
-			            {!! Form::text('titulo', null, ['class' => 'form-control']) !!}
-			        </div>
-	            </div>
-			</div>	
-			<div class="col-md-6">
-				<div class="form-group">
-				{!! Form::label('fecha', 'Fecha', ['class' => 'control-label col-md-4']) !!}
-				<div class="col-md-8">
-					{!! Form::date('fecha', null, ['class' => 'form-control']) !!}
+					{!! Form::label('titulo', 'Nombre feriado', ['class' => 'control-label col-md-4']) !!}
+					<div class="col-md-8">
+						{!! Form::text('titulo', null, ['class' => 'form-control']) !!}
+					</div>
 				</div>
 			</div>
+			<div class="col-md-6">
+				<div class="form-group">
+					{!! Form::label('fecha', 'Fecha', ['class' => 'control-label col-md-4']) !!}
+					<div class="col-md-8">
+						{!! Form::date('fecha', null, ['class' => 'form-control']) !!}
+					</div>
+				</div>
 			</div>
+			<div class="row">
+				<div class="col-md-6">
+					<div class="pull-left">
+						<a class="btn btn-raised btn-primary" href="{{route('calendario.index')}}" >Salir</a>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="form-group" style="margin-top: 0;">
+						<div class="pull-right">
+							<button type="submit" class="btn btn-raised btn-primary">Guardar</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			{!! Form::close() !!}
 		</div>
 	</div>
 @endsection
