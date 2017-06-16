a662244bfa1b6270a8fc3c5aa3fcd1da94818ac5
markoz1 <marco.jhoel@gmail.com>
2017-06-16 09:32:36 -0400

    correcciones visuales en configuracion calendario

--- resources/views/calendario/config.blade.php
+++ resources/views/calendario/config.blade.php
@@ -15,41 +15,42 @@
 						<div class="col-md-12" style="margin-top: 10px;">
 							@if($errors->has('gestion'))
 								<div class="form-group has-error">
-									<div class="help-block">
-										{{ $errors->first('gestion') }}
-									</div>
 							@else
 								<div class="form-group">
 							@endif
 									{!! Form::label('gestion', 'Ingrese la gestion', ['class' => 'control-label col-md-4']) !!}
 									<div class="col-md-5">
 										{!! Form::text('gestion', '1-20XX', ['class' => 'form-control']) !!}
+										<div class="help-block">
+											{{ $errors->first('gestion') }}
+										</div>
 									</div>
 								</div>
 							@if($errors->has('fecha_inicial'))
 								<div class="form-group has-error">
-									<div class="help-block">
-										{{ $errors->first('fecha_inicial') }}
-									</div>
 							@else
 								<div class="form-group">
 							@endif
 									{!! Form::label('fecha_inicial', 'Fecha Inicial', ['class' => 'control-label col-md-4']) !!}
 									<div class="col-md-5">
 										{!! Form::date('fecha_inicial', null, ['class' => 'form-control']) !!}
+										<div class="help-block">
+											{{ $errors->first('fecha_inicial') }}
+										</div>
 									</div>
 								</div>
 							@if($errors->has('fecha_final'))
 								<div class="form-group has-error">
-									<div class="help-block">
-										{{ $errors->first('fecha_final') }}
-									</div>
+										
 							@else
 								<div class="form-group">
 							@endif
 									{!! Form::label('fecha_final', 'Fecha Final', ['class' => 'control-label col-md-4']) !!}
 									<div class="col-md-5">
 										{!! Form::date('fecha_final', null, ['class' => 'form-control']) !!}
+										<div class="help-block">
+											{{ $errors->first('fecha_final') }}
+										</div>
 									</div>
 								</div>
 						</div>
@@ -66,30 +67,30 @@
 								<div class="col-md-6">
 									@if($errors->has('primer_parcial_ini'))
 										<div class="form-group has-error">
-											<div class="help-block">
-												{{ $errors->first('primer_parcial_ini') }}
-											</div>
 									@else
 										<div class="form-group">
 									@endif
 											{!! Form::label('primer_parcial_ini', 'Fecha Inicial', ['class' => 'control-label col-md-4']) !!}
 											<div class="col-md-7">
 												{!! Form::date('primer_parcial_ini', null, ['class' => 'form-control']) !!}
+												<div class="help-block">
+													{{ $errors->first('primer_parcial_ini') }}
+												</div>
 											</div>
 										</div>
 								</div>
 								<div class="col-md-6">
 									@if($errors->has('primer_parcial_fin'))
 										<div class="form-group has-error">
-											<div class="help-block">
-												{{ $errors->first('primer_parcial_fin') }}
-											</div>
 									@else
 										<div class="form-group">
 									@endif
 											{!! Form::label('primer_parcial_fin', 'Fecha Final', ['class' => 'control-label col-md-4']) !!}
 											<div class="col-md-7">
 												{!! Form::date('primer_parcial_fin', null, ['class' => 'form-control']) !!}
+												<div class="help-block">
+													{{ $errors->first('primer_parcial_fin') }}
+												</div>
 											</div>
 										</div>
 								</div>
@@ -99,30 +100,30 @@
 								<div class="col-md-6">
 									@if($errors->has('segundo_parcial_ini'))
 										<div class="form-group has-error">
-											<div class="help-block">
-												{{ $errors->first('segundo_parcial_ini') }}
-											</div>
 									@else
 										<div class="form-group">
 									@endif
 											{!! Form::label('segundo_parcial_ini', 'Fecha Inicial', ['class' => 'control-label col-md-4']) !!}
 											<div class="col-md-7">
 												{!! Form::date('segundo_parcial_ini', null, ['class' => 'form-control']) !!}
+												<div class="help-block">
+													{{ $errors->first('segundo_parcial_ini') }}
+												</div>
 											</div>
 										</div>
 								</div>
 								<div class="col-md-6">
 									@if($errors->has('segundo_parcial_fin'))
 										<div class="form-group has-error">
-											<div class="help-block">
-												{{ $errors->first('segundo_parcial_fin') }}
-											</div>
 									@else
 										<div class="form-group">
 									@endif
 											{!! Form::label('segundo_parcial_fin', 'Fecha Final', ['class' => 'control-label col-md-4']) !!}
 											<div class="col-md-7">
 												{!! Form::date('segundo_parcial_fin', null, ['class' => 'form-control']) !!}
+												<div class="help-block">
+													{{ $errors->first('segundo_parcial_fin') }}
+												</div>
 											</div>
 										</div>
 								</div>
