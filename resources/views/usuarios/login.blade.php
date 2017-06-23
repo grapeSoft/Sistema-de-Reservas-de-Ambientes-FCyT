f6bee770b83241c369b03840f25ebb2d3b62272b
markoz1 <marco.jhoel@gmail.com>
2017-06-22 23:41:38 -0400

    modificaciones vista login

--- resources/views/usuarios/login.blade.php
+++ resources/views/usuarios/login.blade.php
@@ -1,43 +1,48 @@
 @extends('plantillas.principal')
 @section('contenido')
+<div class="hidden-xs" style="padding-bottom: 100px;">
+</div>
 <div class="container">
 	<div class="col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4">
 		<div class="panel panel-primary">
 			<div class="panel-heading text-center">
-				<h4>Iniciar sesión</h4>
+				<h3>Sistema de Reservas <small style="color: #fff;">FCyT - UMSS</small></h3> 
 			</div>
 			<div class="panel-body">
+				<h2 class="text-center">Iniciar Sesión</h2>
 				@if(session('mensaje'))
-				<div class="alert alert-success">
+				<div class="alert alert-dismissible alert-success">
+					<button type="button" class="close" data-dismiss="alert">×</button>
 					{{ session('mensaje') }}
 				</div>
 				@endif
-
 				@if($errors->has('login'))
-				<div class="alert alert-danger" role="alert">
+				<div class="alert alert-dismissible alert-danger">
+					<button type="button" class="close" data-dismiss="alert">×</button>
 					{{ $errors->first('login') }}
 				</div>
 				@endif
 				{!! Form::open(['route' => 'usuarios.logear']) !!}
-					<div class="form-group">
-						<!-- {!! Form::label('username', 'Nombre de usuario', ['class' => 'control-label']) !!} -->
-						{!! Form::text('username', null, ['class' => 'form-control', 'placeholder' => 'Nombre de usuario']) !!}
+					<div class="form-group label-floating">
+						{!! Form::label('username', 'Nombre de usuario', ['class' => 'control-label']) !!}
+						{!! Form::text('username', null, ['class' => 'form-control']) !!}
 					</div>
-					<div class="form-group">
-						<!-- {!! Form::label('password', 'Contraseña', ['class' => 'control-label']) !!} -->
-						{!! Form::password('password', ['class' => 'form-control', 'placeholder' => 'Contraseña']) !!}
+					<div class="form-group label-floating">
+						{!! Form::label('password', 'Contraseña', ['class' => 'control-label']) !!}
+						{!! Form::password('password', ['class' => 'form-control']) !!}
 					</div>
 					<div>
 						<a href="{{ route('usuarios.recuperarC') }}" class="label-link"> Olvidaste tu contraseña? </a>
 					</div>
 					<div class="text-center">
-						<button type="submit" class="btn btn-primary">Ingresar</button>
+						<button type="submit" class="btn btn-primary btn-raised">Ingresar</button>
 					</div>
 				{!! Form::close() !!}
 			</div>
 			<div class="panel-footer text-center">
-				<h5>Sistema de Reservas para la FCyT - UMSS</h5>
-				<h6>Copyright &copy;2017</h6>
+				<h5>Copyright &copy;2017 
+				<a href="mailto:grapesoftbolivia@gmail.com" class="label-link"> GrapeSoft. </a>
+				Todos los derechos reservados</h5>
 			</div>
 		</div>
 	</div>
